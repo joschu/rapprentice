switched kernel matrix sign (unit tests pass, but there's probably various stuff wrong)

rapprentice/tps.py
@@ -11,18 +11,38 @@ from rapprentice.colorize import colorize
 VERBOSE = False
 ENABLE_SLOW_TESTS = False
 
+def tps_apply_kernel(distmat, dim):
+    if dim==2:
+        return distmat**2 * np.log(distmat)
+    elif dim ==3:
+        return -distmat
+    else:
+        raise NotImplementedError
+    
+    
+def tps_kernel_matrix(x_na):
+    dim = x_na.shape[1]
+    distmat = ssd.squareform(ssd.pdist(x_na))
+    return tps_apply_kernel(distmat,dim)
+
+def tps_kernel_matrix2(x_na, y_ma):
+    dim = x_na.shape[1]
+    distmat = ssd.cdist(x_na, y_ma)
+    return tps_apply_kernel(distmat, dim)
+
 def nan2zero(x):
     np.putmask(x, np.isnan(x), 0)
     return x
 
 def tps_eval(x_ma, lin_ag, trans_g, w_ng, x_na):
-    K_mn = ssd.cdist(x_ma, x_na)
+    K_mn = tps_kernel_matrix2(x_ma, x_na)
     return np.dot(K_mn, w_ng) + np.dot(x_ma, lin_ag) + trans_g[None,:]
 
 def tps_grad(x_ma, lin_ag, _trans_g, w_ng, x_na):
     _N, D = x_na.shape
     M = x_ma.shape[0]
 
+    assert x_ma.shape[1] == 3
     dist_mn = ssd.cdist(x_ma, x_na,'euclidean')
 
     grad_mga = np.empty((M,D,D))
@@ -30,7 +50,7 @@ def tps_grad(x_ma, lin_ag, _trans_g, w_ng, x_na):
     lin_ga = lin_ag.T
     for a in xrange(D):
         diffa_mn = x_ma[:,a][:,None] - x_na[:,a][None,:]
-        grad_mga[:,:,a] = lin_ga[None,:,a] + np.dot(nan2zero(diffa_mn/dist_mn),w_ng)
+        grad_mga[:,:,a] = lin_ga[None,:,a] - np.dot(nan2zero(diffa_mn/dist_mn),w_ng)
     return grad_mga
     
 def tps_nr_grad(x_ma, lin_ag, _trans_g, w_ng, x_na, return_tuple = False):
@@ -40,16 +60,17 @@ def tps_nr_grad(x_ma, lin_ag, _trans_g, w_ng, x_na, return_tuple = False):
     N, D = x_na.shape
     M = x_ma.shape[0]
 
+    assert x_ma.shape[1] == 3
     dists_mn = ssd.cdist(x_ma, x_na,'euclidean')
     diffs_mna = x_ma[:,None,:] - x_na[None,:,:]
 
     grad_mga = np.empty((M,D,D))
     lin_ga = lin_ag.T
     for a in xrange(D):
-        grad_mga[:,:,a] = lin_ga[None,:,a] + np.dot(nan2zero(diffs_mna[:,:,a]/dists_mn),w_ng)
+        grad_mga[:,:,a] = lin_ga[None,:,a] - np.dot(nan2zero(diffs_mna[:,:,a]/dists_mn),w_ng)
 
     # m n g a b
-    Jw_mngab = (nan2zero(diffs_mna[:,:,None,:,None]/dists_mn[:,:,None,None,None])) * grad_mga[:,None,:,None,:]
+    Jw_mngab = - (nan2zero(diffs_mna[:,:,None,:,None]/dists_mn[:,:,None,None,None])) * grad_mga[:,None,:,None,:]
     Jw_mngab = Jw_mngab + Jw_mngab.transpose(0,1,2,4,3)        
     Jw_mabng = Jw_mngab.transpose(0,3,4,1,2)
     Jw = Jw_mabng.reshape(M*D**2,N*D)
@@ -84,7 +105,7 @@ def tps_cost(lin_ag, trans_g, w_ng, x_na, y_ng, bend_coef, K_nn=None, return_tup
     XXX doesn't include rotation cost
     """
     D = lin_ag.shape[0]
-    if K_nn is None: K_nn = ssd.squareform(ssd.pdist(x_na))
+    if K_nn is None: K_nn = tps_kernel_matrix(x_na)
     ypred_ng = np.dot(K_nn, w_ng) + np.dot(x_na, lin_ag) + trans_g[None,:]
     res_cost = ((ypred_ng - y_ng)**2).sum()
     bend_cost = bend_coef * sum(np.dot(w_ng[:,g], np.dot(-K_nn, w_ng[:,g])) for g in xrange(D))
@@ -95,10 +116,10 @@ def tps_cost(lin_ag, trans_g, w_ng, x_na, y_ng, bend_coef, K_nn=None, return_tup
 
 def tps_nr_cost_eval(lin_ag, trans_g, w_ng, x_na, y_ng, xnr_ma, bend_coef, nr_coef, K_nn = None, return_tuple=False):
     D = lin_ag.shape[0]
-    if K_nn is None: K_nn = ssd.squareform(ssd.pdist(x_na))
+    if K_nn is None: K_nn = tps_kernel_matrix(x_na)
     ypred_ng = np.dot(K_nn, w_ng) + np.dot(x_na, lin_ag) + trans_g[None,:]
     res_cost = ((ypred_ng - y_ng)**2).sum()
-    bend_cost = bend_coef * sum(np.dot(w_ng[:,g], np.dot(-K_nn, w_ng[:,g])) for g in xrange(D))
+    bend_cost = bend_coef * sum(np.dot(w_ng[:,g], np.dot(K_nn, w_ng[:,g])) for g in xrange(D))
     nr_cost = nr_coef * (tps_nr_err(xnr_ma, lin_ag, trans_g, w_ng, x_na)**2).sum()
     if return_tuple:
         return res_cost, bend_cost, nr_cost, res_cost + bend_cost + nr_cost
@@ -111,7 +132,7 @@ def tps_nr_cost_eval_general(lin_ag, trans_g, w_eg, x_ea, y_ng, nr_ma, bend_coef
     M = nr_ma.shape[0]
     assert E == N+4*M
     
-    K_ee = K_ee or ssd.squareform(ssd.pdist(x_ea))
+    K_ee = K_ee or tps_kernel_matrix(x_ea)
     K_ne = K_ee[:N]
     x_na = x_ea[:N]
     
@@ -128,7 +149,7 @@ def tps_fit(x_na, y_ng, bend_coef, rot_coef, wt_n=None, K_nn = None):
     N,D = x_na.shape
         
     # XXX wt not used
-    K_nn = ssd.squareform(ssd.pdist(x_na)) if K_nn is None else K_nn
+    K_nn = tps_kernel_matrix(x_na) if K_nn is None else K_nn
     coef_ratio = bend_coef / rot_coef if rot_coef > 0 else 0
     #if wt_n is None: reg_nn = bend_coef * np.eye(N)    
     #else: reg_nn = np.diag(bend_coef/(wt_n + 1e-6))
@@ -138,7 +159,7 @@ def tps_fit(x_na, y_ng, bend_coef, rot_coef, wt_n=None, K_nn = None):
     
     A[:N, :N] = K_nn
 
-    A.flat[np.arange(0,N)*(N+D+2)] -= bend_coef/(wt_n if wt_n is not None else 1)
+    A.flat[np.arange(0,N)*(N+D+2)] += bend_coef/(wt_n if wt_n is not None else 1)
 
     A[:N, N:N+D] = x_na
     A[:N, N+D] = 1
@@ -191,12 +212,12 @@ def tps_fit3(x_na, y_ng, bend_coef, rot_coef, wt_n):
     if wt_n is None: wt_n = np.ones(len(x_na))
     n,d = x_na.shape
     assert d == 3
-    K_nn = ssd.squareform(ssd.pdist(x_na))
+    K_nn = tps_kernel_matrix(x_na)
     Q = np.c_[np.ones((n,1)), x_na, K_nn]
     WQ = wt_n[:,None] * Q
     QWQ = Q.T.dot(WQ)
     H = QWQ
-    H[4:,4:] -= bend_coef * K_nn # -K_nn is conditionally positive definite (CPD)
+    H[4:,4:] += bend_coef * K_nn # -K_nn is conditionally positive definite (CPD)
     H[1:4, 1:4] += rot_coef * np.eye(3)
     
     f = -WQ.T.dot(y_ng)
@@ -218,12 +239,12 @@ def tps_fit2(x_na, y_ng, bend_coef, rot_coef, wt_n=None):
     N,D = x_na.shape 
     _u,_s,_vh = np.linalg.svd(np.c_[x_na, np.ones((N,1))], full_matrices=True)
     N_nq = _u[:,4:] # null of data
-    K_nn = ssd.squareform(ssd.pdist(x_na))
+    K_nn = tps_kernel_matrix(x_na)
     Q_nn = np.c_[x_na, np.ones((N,1)),K_nn.dot(N_nq)]
     QQ_nn = np.dot(Q_nn.T, Q_nn)
     
     A = QQ_nn    
-    A[4:, 4:] -= bend_coef * N_nq.T.dot(K_nn).dot(N_nq)
+    A[4:, 4:] += bend_coef * N_nq.T.dot(K_nn).dot(N_nq)
     B = Q_nn.T.dot(y_ng)
 
     A[:3, :3] += rot_coef * np.eye(3)
@@ -253,7 +274,7 @@ def tps_nr_fit(x_na, y_ng, bend_coef, nr_ma, nr_coef, method="newton"):
     N_nq = _u[:,4:] # null of data
     #w_ng = N_nq.dot(N_nq.T.dot(w_ng))
         
-    K_nn = ssd.squareform(ssd.pdist(x_na))
+    K_nn = tps_kernel_matrix(x_na)
     Q_nn = np.c_[x_na, np.ones((N,1)),K_nn.dot(N_nq)]
     QQ_nn = np.dot(Q_nn.T, Q_nn)
     Bend_nn = np.zeros((N,N))
@@ -369,7 +390,7 @@ def tps_nr_fit_enhanced(x_na, y_ng, bend_coef, nr_ma, nr_coef, plotting=0):
     # e is number of kernels
     # q is number of nonrigid dofs
     # f is total number of dofs
-    K_ee = ssd.squareform(ssd.pdist(x_ea))
+    K_ee = tps_kernel_matrix(x_ea)
     K_ne = K_ee[:N, :]
     Q_nf = np.c_[x_na, np.ones((N,1)),K_ne.dot(N_eq)]
     QQ_ff = np.dot(Q_nf.T, Q_nf)
@@ -439,13 +460,13 @@ def tps_fit_fixedrot(x_na, y_ng, bend_coef, lin_ag, K_nn = None, wt_n=None):
     N,_D = x_na.shape
     _u,_s,_vh = np.linalg.svd(np.c_[x_na, np.ones((N,1))], full_matrices=True)
     N_nq = _u[:,4:] # null of data
-    K_nn = ssd.squareform(ssd.pdist(x_na))
+    K_nn = tps_kernel_matrix(x_na)
 
     Q_nn = np.c_[np.ones((N,1)),K_nn.dot(N_nq)]
     QQ_nn = np.dot(Q_nn.T, Q_nn)
     
     A = QQ_nn
-    A[1:, 1:] -= bend_coef * N_nq.T.dot(K_nn).dot(N_nq)
+    A[1:, 1:] += bend_coef * N_nq.T.dot(K_nn).dot(N_nq)
     B = Q_nn.T.dot(y_ng-x_na.dot(lin_ag))
 
     X = np.linalg.solve(A,B)
@@ -464,7 +485,7 @@ def tps_fit_regrot(x_na, y_ng, bend_coef, rfunc, wt_n=None, max_iter = 1, inner_
 
 
     N,D = x_na.shape
-    K_nn = ssd.squareform(ssd.pdist(x_na))
+    K_nn = tps_kernel_matrix(x_na)
     # initialize with tps_fit and small rotation regularization
     if l_init is None: 
         lin_ag, trans_g, w_ng = tps_fit3(x_na, y_ng, bend_coef, .01, wt_n)