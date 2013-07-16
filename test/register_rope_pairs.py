a8b696e45232f19f12f3bd7827bbcb2b90c7017e
John Schulman <john.d.schulman@gmail.com>
2013-07-16 11:49:05 -0700

    played with downsampling

--- test/register_rope_pairs.py
+++ test/register_rope_pairs.py
@@ -1,24 +1,29 @@
 import h5py
 import rapprentice.registration as reg, rapprentice.cloud_proc_funcs as cpf
 from joblib import Memory, Parallel, delayed
+import openravepy
 import cPickle, h5py, os.path as osp, numpy as np, scipy.spatial.distance as ssd
 from time import time
 
 aa = np.asarray
-mem = Memory(cachedir='/tmp/joblib')
+mem = Memory(cachedir='/tmp/joblib1')
 
-cached_extract_red = mem.cache(cpf.extract_red)
-
+# cached_extract_red = mem.cache(cpf.extract_red)
+cached_extract_red = cpf.extract_red
 @mem.cache(ignore=["hdf"])
 def extract_clouds(hdf):
-    return [cached_extract_red(aa(seg["rgb"]), aa(seg["depth"]), aa(seg["T_w_k"])) for (key,seg) in hdf.items()]
+    results = []
+    for (key,seg) in hdf.items():
+        print key
+        results.append(cached_extract_red(aa(seg["rgb"]), aa(seg["depth"]), aa(seg["T_w_k"])))
+    return results
 
 if __name__ == "__main__":
-    hdf = h5py.File("/Users/joschu/Data/knots/master.h5", "r")
+    hdf = h5py.File("/Users/joschu/Data/all.h5", "r")
     clouds = extract_clouds(hdf)    
 
 
-    dim = 2
+    dim = 3
     
 
     for cloud0 in clouds:
@@ -38,28 +43,42 @@ if __name__ == "__main__":
             cost = reg.tps_reg_cost(fest_scaled) + reg.tps_reg_cost(gest_scaled)
             print "cost: %.3f"%cost
 
-            import matplotlib.pyplot as plt
-            plt.clf()
-            # plt.plot(xys[:,1], xys[:,0],'r.')
-            # plt.plot(fxys[:,1], fxys[:,0],'b.')
-            # plt.plot(fxys_est[:,1], fxys_est[:,0],'g.')
-
+            
+            
             scaled_fcloud = fest_scaled.transform_points(scaled_cloud0)
-            plt.plot(scaled_cloud0[:,1], scaled_cloud0[:,0],'r.')
-            plt.plot(scaled_cloud1[:,1], scaled_cloud1[:,0],'b.')
-            plt.plot(scaled_fcloud[:,1], scaled_fcloud[:,0],'g.')
-            
-            
+        
             if dim == 2: 
+                import matplotlib.pyplot as plt
+                plt.clf()
+                plt.figure(1)
+                # plt.plot(xys[:,1], xys[:,0],'r.')
+                # plt.plot(fxys[:,1], fxys[:,0],'b.')
+                # plt.plot(fxys_est[:,1], fxys_est[:,0],'g.')
+
+                plt.plot(scaled_cloud0[:,1], scaled_cloud0[:,0],'r.')
+                plt.plot(scaled_cloud1[:,1], scaled_cloud1[:,0],'b.')
+                plt.plot(scaled_fcloud[:,1], scaled_fcloud[:,0],'g.')
                 from rapprentice.plotting_plt import plot_warped_grid_2d
                 plot_warped_grid_2d(fest_scaled.transform_points, [-.5,-.5], [.5,.5])
+                plt.draw()
+                raw_input()
             
             
             elif dim == 3:
                 env= openravepy.RaveGetEnvironment(1)
                 if env is None: env = openravepy.Environment()
+                import trajoptpy
+                viewer = trajoptpy.GetViewer(env)
+                if env is None: env = openravepy.Environment()
                 from rapprentice.plotting_openrave import draw_grid
-                draw_grid(fest_scaled, [-.5,-.5,-.5], [.5,.5,.5])
+                handles = []
+                handles.append(env.plot3(scaled_cloud0,5,(1,0,0,1)))
+                handles.append(env.plot3(scaled_cloud1,5,(0,0,1,1)))
+                handles.append(env.plot3(scaled_fcloud,5,(0,1,0,1)))
+
+                handles.extend(draw_grid(env, fest_scaled.transform_points, [-.5,-.5,-.1], [.5,.5,.1]))
+                viewer.Idle()
             
-            plt.draw()
-            raw_input()
+            
+
+
--- test/test_registration_synthetic.py
+++ test/test_registration_synthetic.py
@@ -28,7 +28,57 @@ def voxel_downsample(xyz, s):
         x,y,z = pt
         d[(int(x/s), int(y/s), int(z/s))].append(i)
     return np.array([xyz[inds].mean(axis=0) for inds in d.values()])
+def kmeans_downsample(xyz, n):
+    from scipy.cluster import vq 
+    centroids, label = vq.kmeans2(xyz,n)
+    return centroids[np.bincount(label) > 0]
+    return centroids
+    
+def compute_centroids(xyzs, clus):
+    n = len(xyzs)
+    assert len(clus) == n
+    id2count = np.zeros((n))
+    id2sum = np.zeros((n, xyzs.shape[1]))
+    for (clu,xyz) in zip(clus, xyzs):
+        id2count[clu] += 1
+        id2sum[clu] += xyz
+        
+    clusters = []
+    for i in xrange(n):
+        if id2count[i] > 0:
+            clusters.append(id2sum[i]/id2count[i])
+    print np.array(clusters)
+    return np.array(clusters)
+        
+    
+def hclust_downsample(xyz, n):
+    import scipy.spatial.distance as ssd
+    D = ssd.pdist(xyz)
+    from scipy.cluster import hierarchy
+    Z = hierarchy.linkage(D, method='complete',metric='euclidean')
+    clus = hierarchy.fcluster(Z, t=n, criterion='maxclust')
+    return compute_centroids(xyz, clus)
 
+def plot_cb(x_nd, y_md, xtarg_nd, corr_nm, wt_n, f):
+    import matplotlib.pyplot as plt
+    plt.clf()
+    # plt.plot(xys[:,1], xys[:,0],'r.')
+    # plt.plot(fxys[:,1], fxys[:,0],'b.')
+    # plt.plot(fxys_est[:,1], fxys_est[:,0],'g.')
+
+    fx_nd = f.transform_points(x_nd)
+    plt.plot(x_nd[:,1], x_nd[:,0],'r.')
+    plt.plot(fx_nd[:,1], fx_nd[:,0],'g.')
+    plt.plot(y_md[:,1], y_md[:,0],'b.')
+
+    def to2d(f):
+        def f2d(x):
+            return f(np.c_[x, np.zeros((len(x),1))])[:,:2]
+        return f2d
+    transform_func = to2d(f.transform_points) if args.to3d else f.transform_points
+    plot_warped_grid_2d(transform_func, [-.5,-.5], [.5,.5])
+    plt.draw()
+    plt.ginput()    
 
 def registration(xys, fxys):
     if args.to3d:
@@ -40,14 +90,16 @@ def registration(xys, fxys):
     scaled_fxys, targ_params = reg.unit_boxify(fxys)
     
     downsample = voxel_downsample if args.to3d else pixel_downsample
-    scaled_ds_xys = downsample(scaled_xys, .03)
-    scaled_ds_fxys = downsample(scaled_fxys, .03)
+    scaled_ds_xys = downsample(scaled_xys, .01)
+    scaled_ds_fxys = downsample(scaled_fxys, .01)
+    scaled_ds_xys = hclust_downsample(scaled_ds_xys, 100)
+    scaled_ds_fxys = hclust_downsample(scaled_ds_fxys, 101)
     
     print "downsampled to %i and %i pts"%(len(scaled_ds_xys),len(scaled_ds_fxys))
     tstart = time()
     
     # fest_scaled = reg.tps_rpm(scaled_ds_xys, scaled_ds_fxys, n_iter=10, reg_init = 10, reg_final=.01)
-    fest_scaled,_ = reg.tps_rpm_bij(scaled_ds_xys, scaled_ds_fxys, n_iter=10, reg_init = 10, reg_final=.01)
+    fest_scaled,_ = reg.tps_rpm_bij(scaled_ds_xys, scaled_ds_fxys, n_iter=50, reg_init = 10, reg_final=.01, rad_init=.1, rad_final=.01, plot_cb = plot_cb if args.plotting else None, plotting = 0 if args.plotting else 0)
     
     
     
@@ -56,27 +108,7 @@ def registration(xys, fxys):
     fxys_est = fest.transform_points(xys)
     if len(fxys_est) == len(fxys): print "error:", np.abs(fxys_est - fxys).mean()
     
-    if args.plotting:
-        import matplotlib.pyplot as plt
-        plt.clf()
-        # plt.plot(xys[:,1], xys[:,0],'r.')
-        # plt.plot(fxys[:,1], fxys[:,0],'b.')
-        # plt.plot(fxys_est[:,1], fxys_est[:,0],'g.')
-
-        scaled_ds_fxys_est = fest_scaled.transform_points(scaled_ds_xys)
-        plt.plot(scaled_ds_xys[:,1], scaled_ds_xys[:,0],'r.')
-        plt.plot(scaled_ds_fxys[:,1], scaled_ds_fxys[:,0],'b.')
-        plt.plot(scaled_ds_fxys_est[:,1], scaled_ds_fxys_est[:,0],'g.')
-
-
-        def to2d(f):
-            def f2d(x):
-                return f(np.c_[x, np.zeros((len(x),1))])[:,:2]
-            return f2d
-        transform_func = to2d(fest_scaled.transform_points) if args.to3d else fest_scaled.transform_points
-        plot_warped_grid_2d(transform_func, [-.5,-.5], [.5,.5])
-        plt.draw()
-        plt.ginput()
+    if args.plotting: plot_cb(scaled_ds_xys, scaled_ds_fxys, None,None,None,fest_scaled)
         
 def main():
     
